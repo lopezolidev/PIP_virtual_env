b524b40afb32dd16a55bd04754fdd095d8edf174
lopezolidev <olivera9806@gmail.com>
2024-03-30 16:48:00 -0400

    add files: deployed FastAPI server with uvicorn and run a few lines of html responde

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1, 2, 3, 4]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_contact():
+    return """
+    <h1>hello I'm a website</h1>
+    <p>And I'm a paragraph</p>
+    """
 
 def run():
     store.get_catgories()
